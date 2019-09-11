8c0cf2797d58fc8b3c366bc0ffa053080dbd23bd
aluno <aluno>
2019-09-11 12:08:51 -0300

    Fase 1: concluída!

---
+++ banco.sql.sql
@@ -0,0 +1,11 @@
+create database  if not exists Contadores ;
+use Contadores; 
+CREATE TABLE if not exists contador
+(
+Nome varchar(15) NOT NUll,
+Numero char (15) NOT NUll,
+Codigo int  (15) NOT NUll,
+primary key (Codigo)
+);
+
+
---
+++ comandos.sql
@@ -0,0 +1,7 @@
+use contador;
+
+select * from contador;
+insert contador values('xixi', 3, 1); 
+insert contador values('agua', 8, 2);
+update contador set Nome = "copo d'agua" where Codigo = 2;
+update contador set Numero = 9 where Codigo = 2;
