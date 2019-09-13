7e3426c24eadce30f17cb42a8737f35f8a5f3550
aluno <aluno>
2019-09-13 15:40:44 -0300

    falta terminar o css

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="contador.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Contador App</title>
+</head>
+<body>
+	<header>
+		<h1>Contador de Coisas</h1>
+	</header>
+	<div id="container">
+		<main>
+			<form action="contarcoisas.php" method="post">
+			<p><button>⊖</button> 
+					<p> xixi </p> 
+					<p>2</p>
+			<button>⊕</button>
+			</p>
+
+			<p>
+			<button>⊖</button> 
+					<p> Copos D'água </p>
+					<p>4</p>
+			<button>⊕</button>
+			</p>
+
+			<p>
+			<button>⊖</button> 
+					<p> Biscoitos </p> 
+					<p>7</p>
+			<button>⊕</button>
+			</p>
+
+
+		</main>
+		<hr>
+		<footer>
+			<p>
+         <label for="icont">Novo contador</label> </p>
+
+         <p><input type="nome" id="icont" name="cont">
+         <button id="add" type="submit">Adicionar</button></p>
+    	 
+		</footer>
+	</div>
+</body>
+</html>
